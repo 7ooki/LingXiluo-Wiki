8c089a69b6c2bc33e20bcb45088338a3c4a36a9b
Sakura <254164579@qq.com>
2025-08-21 15:56:09 +0800

    美化

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,8 @@
+/* .vitepress/theme/index.ts */
+import DefaultTheme from 'vitepress/theme'
+import './style/index.css'
+import './style/rainbow.css'
+
+export default {
+  extends: DefaultTheme,
+}
